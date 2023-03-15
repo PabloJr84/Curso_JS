b7196a3ee2f27110ed75c7d56d53a3fa38eb817d
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-15 18:56:00 -0300

    6ª Atualização JS

--- FUNDAMENTOS/inicio/comentario.js
+++ FUNDAMENTOS/inicio/comentario.js
@@ -1,4 +1,12 @@
 //console.log é usado para exibir algo na tela do computador!
 console.log("Passo #01");
 console.log("Passo #02");
-console.log("Passo #03");
+console.log("Passo #03");// mais um exemplo de console.log!
+//console.log("Passo #04");
+/*
+Esse é um
+comentário 
+de múltlipas 
+linhas
+*/
+console.log("Passo #05")
