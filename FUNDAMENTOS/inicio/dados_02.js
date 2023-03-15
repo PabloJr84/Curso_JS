2a0db8d71675ba6dcaa5bcb3135e3e57cbbe785a
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-15 20:58:21 -0300

    10ª Atulização JS

---
+++ FUNDAMENTOS/inicio/dados_02.js
@@ -0,0 +1,7 @@
+console.log(19.9 * 0.6);
+
+var preco = 19.90;
+var desconto = 0.4;
+var precoComDesconto = preco * (1 - desconto);
+console.log (precoComDesconto);
+console.log("Preço com desconteo é : " + precoComDesconto)
