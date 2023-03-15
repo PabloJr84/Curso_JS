d9d00789b8afe98e98a5032a65bfa26f32903da6
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-15 19:57:21 -0300

    8ª Atualização JS

---
+++ FUNDAMENTOS/inicio/desafio_senca_bloco.js
@@ -0,0 +1,35 @@
+// Desaafio para apresentar numero ímpares e numeros pares na tela
+/*
+Bloco 
+1
+3
+5
+7
+9
+
+Bloco 2
+
+2
+4
+6
+8
+10
+*/
+{
+    console.log("Bloco 1 numeros Ímapres")
+    console.log (" Numero 01");
+    console.log (" Numero 03");
+    console.log (" Numero 05");
+    console.log (" Numero 07");
+    console.log (" Numero 09");
+   
+}
+
+{
+    console.log("Bloco 2 numeros pares");
+    console.log (" Numero 02");
+    console.log (" Numero 04");
+    console.log (" Numero 06");
+    console.log (" Numero 08");
+    console.log (" Numero 10");
+}
