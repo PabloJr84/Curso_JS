be4844f220a9d7d8d86d1ddce4e25c3fd4c34fd6
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-15 19:38:58 -0300

    7ª Atualização JS

---
+++ FUNDAMENTOS/inicio/bloco.js
@@ -0,0 +1,13 @@
+//Um bloco é um local exato onde vamos agrupar senteça de codigo
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+// Podemos fazer blocos dentro de outros blocos
+{
+    console.log("Passo #04");
+    console.log("Passo 05");
+}
+
+console.log(" Fim");
