f6a68d9c70adb41fb667d0e5e40b54f03c5d9754
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-16 19:46:25 -0300

    13ª Atualização

---
+++ FUNDAMENTOS/inicio/dados_03.js
@@ -0,0 +1,12 @@
+var idade = 31;
+console.log (typeof 31);
+console.log (typeof idade);
+
+var salario = 4578.32;
+console.log(typeof salario );
+
+var estaChovendo = true // ou false
+console.log ( typeof estaChovendo);
+
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
