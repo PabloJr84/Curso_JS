e367a3dff0a08e1a46b90d0395c5f25c56631451
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-15 21:24:39 -0300

    11ª Atualização JS

--- FUNDAMENTOS/inicio/dados_02.js
+++ FUNDAMENTOS/inicio/dados_02.js
@@ -1,6 +1,6 @@
 console.log(19.9 * 0.6);
 
-var preco = 19.90;
+var preco = 19.90; // String (texto)-> sequencia de símbolos
 var desconto = 0.4;
 var precoComDesconto = preco * (1 - desconto);
 console.log (precoComDesconto);
