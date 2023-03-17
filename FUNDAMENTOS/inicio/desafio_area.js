2d45da286a28fe78f6d1c978f88dbd5e29c137bf
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-16 23:22:06 -0300

    15ª Atualização JS

---
+++ FUNDAMENTOS/inicio/desafio_area.js
@@ -0,0 +1,16 @@
+/*
+Desafio é fazer o calculo de PI
+
+*/ 
+
+const PI = 3.141592;
+var raio = 10;
+var areaCirc = PI * raio * raio;
+
+console.log (" O valor da área é " + areaCirc + "m2.");
+
+var idade = 55;
+var idade2 = 15;
+
+var resultado = idade + idade2;
+console.log(" O valor da idade somadas são : " + resultado);
