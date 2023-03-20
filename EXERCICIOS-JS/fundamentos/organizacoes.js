9179f7875dea9bfb275ef5f9d97c4f4435f9c4f1
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-20 09:18:55 -0300

    Atualização - Sobre curso

---
+++ EXERCICIOS-JS/fundamentos/organizacoes.js
@@ -0,0 +1,9 @@
+console.log("Sentença de código")
+
+{
+
+    {
+    console.log("Testando a sentença de código");
+    console.log("Teste 02 da sentença de codigo") // Padrão do curso , terminar o codigo sem ;
+    }
+}
