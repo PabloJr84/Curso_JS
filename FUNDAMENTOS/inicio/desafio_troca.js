59177e2ee54c12b5344b2922e68645fae6dcca10
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-20 07:23:04 -0300

    Atualização desafio troca de valores

--- FUNDAMENTOS/inicio/desafio_troca.js
+++ FUNDAMENTOS/inicio/desafio_troca.js
@@ -4,6 +4,6 @@ let temp = a
 a = b;
 b = temp;
 
-//Desafio da troca de valores : a = 94 e b = 7
+//Desafio da troca de valores : Depois da troca o... a = 94 e b = 7
 console.log(a);
 console.log(b);
